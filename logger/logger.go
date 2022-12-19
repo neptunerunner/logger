ff75a942cfa4c51fc0d15f5decbc2f34b82c3843
neptunerunner <121019949+neptunerunner@users.noreply.github.com>
2022-12-20 00:50:32 +0800

    adjustment

--- logger/logger.go
+++ logger/logger.go
@@ -1,4 +1,4 @@
-package main
+package logger
 
 import (
 	"fmt"
@@ -6,15 +6,7 @@ import (
 	"os"
 )
 
-func logger(file *os.File) {
-	log.SetOutput(file)
-
-	log.SetFlags(log.Lshortfile | log.LstdFlags)
-
-	log.Println("logging to custom file")
-}
-
-func main() {
+func Logger(file *os.File) {
 	LOG_FILE := "C:\\Users\\frank\\Documents\\logs\\log.txt"
 
 	logFile, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
@@ -24,6 +16,9 @@ func main() {
 	}
 
 	defer logFile.Close()
+	log.SetOutput(file)
 
-	logger(logFile)
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+
+	log.Println("logging to custom file")
 }
